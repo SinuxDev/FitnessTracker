0edb4ad437376081a71c44adb90feefb8aec155e
Aung Ye Htet <aung.atg1738@gmail.com>
2024-04-25 18:58:36 +0630

    Calories Calculate Class Updated!

--- FitnessTracker/CaloriesCalClass.cs
+++ FitnessTracker/CaloriesCalClass.cs
@@ -28,43 +28,33 @@ namespace FitnessTracker
         {
             double caloriesBurned = 0;
 
-            // Perform input validation and calculate calories burned
-            switch (this.Name)
+            //Dictionary to store MET Values for different activities
+            Dictionary<string, double> exerciseMETs = new Dictionary<string, double>()
             {
-                case "Walking":
-                    const double WalkingMET = 3.0;
-                    caloriesBurned = this.Metric2 * WalkingMET * this.Metric3 / 200; // M2(Steps) / M3(Duration = 200 )
-                    break;
-                case "Swimming":
-                    const double SwimmingMET = 8.0;
-                    caloriesBurned = this.Metric1 * SwimmingMET * this.Metric3 / 60; // M1(Times) / M3(Duration = 60 )
-                    break;
-                case "Squat":
-                    // No need to calculate calories burned based on steps for Squat
-                    // Use Metric1 (Times) and Metric3 (Duration)
-                    const double SquatMET = 3.5;
-                    caloriesBurned = this.Metric1 * SquatMET * this.Metric3 / 60; // M1(Times) / M3(Duration = 60 )
-                    break;
-                case "Anaerobic":
-                    // No need to calculate calories burned based on steps for Anaerobic
-                    // Use Metric1 (Times) and Metric3 (Duration)
-                    const double AnaerobicMET = 7.0;
-                    caloriesBurned = this.Metric1 * AnaerobicMET * this.Metric3 / 60; // M1(Times) / M3(Duration = 60)
-                    break;
-                case "Push up":
-                    // No need to calculate calories burned based on steps for Push up
-                    // Use Metric1 (Times) and Metric3 (Duration)
-                    const double PushUpMET = 3.8;
-                    caloriesBurned = this.Metric1 * PushUpMET * this.Metric3 / 60; //M1(Times) / M3(Duration = 60 )
-                    break;
-                case "Pull up":
-                    // No need to calculate calories burned based on steps for Pull up
-                    // Use Metric1 (Times) and Metric3 (Duration)
-                    const double PullUpMET = 5.8;
-                    caloriesBurned = this.Metric1 * PullUpMET * this.Metric3 / 60; // M1(Times) / M3(Duration = 60 )
-                    break;
-                default:
-                    break;
+                { "Walking", 3.0 },
+                { "Swimming", 8.0 },
+                { "Squat", 3.5 },
+                { "Anaerobic", 7.0 },
+                { "Push up", 3.8 },
+                { "Pull up", 5.8 },
+            };
+
+            //Check if the exercise is present in the dictionary
+            if (exerciseMETs.ContainsKey(this.Name))
+            {
+                //Retrieve MET value for the exercise
+                double MET = exerciseMETs[this.Name];
+
+                //Calculate calories burned based on exercise type
+                switch (this.Name)
+                {
+                    case "Walking":
+                        caloriesBurned = this.Metric2 * MET * this.Metric3 / 200; // M2(steps) * MET * M3(Duration) / 200
+                        break;
+                    default:
+                        caloriesBurned = this.Metric1 * MET * this.Metric2 * this.Metric3 / 60; //M1(times) * MET * M3(Duration) / 60
+                        break;
+                }
             }
 
             return caloriesBurned;
