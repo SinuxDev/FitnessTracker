fb8ff10b3905111c6034a8d756094cfb4328f0d0
Aung Ye Htet <aung.atg1738@gmail.com>
2024-05-10 22:11:34 +0630

    ForgetPasswordTokenGenerate(S.. Developing)

---
+++ FitnessTracker/forgetPasswordForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace FitnessTracker
+{
+    partial class forgetPasswordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reset_link_email = new System.Windows.Forms.TextBox();
+            this.send_reset_link = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reset_link_email
+            // 
+            this.reset_link_email.Location = new System.Drawing.Point(264, 88);
+            this.reset_link_email.Name = "reset_link_email";
+            this.reset_link_email.Size = new System.Drawing.Size(118, 22);
+            this.reset_link_email.TabIndex = 0;
+            // 
+            // send_reset_link
+            // 
+            this.send_reset_link.Location = new System.Drawing.Point(264, 176);
+            this.send_reset_link.Name = "send_reset_link";
+            this.send_reset_link.Size = new System.Drawing.Size(115, 46);
+            this.send_reset_link.TabIndex = 1;
+            this.send_reset_link.Text = "Send Reset Link";
+            this.send_reset_link.UseVisualStyleBackColor = true;
+            this.send_reset_link.Click += new System.EventHandler(this.send_reset_link_Click);
+            // 
+            // forgetPasswordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.send_reset_link);
+            this.Controls.Add(this.reset_link_email);
+            this.Name = "forgetPasswordForm";
+            this.Text = "forgetPasswordForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox reset_link_email;
+        private System.Windows.Forms.Button send_reset_link;
+    }
+}
---
+++ FitnessTracker/forgetPasswordForm.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using Microsoft.VisualBasic;
+
+namespace FitnessTracker
+{
+    public partial class forgetPasswordForm : Form
+    {
+        //Database Connection String
+        private string connectionString;
+
+        public forgetPasswordForm(string dbConnectionString)
+        {
+            InitializeComponent();
+
+            connectionString = dbConnectionString;
+        }
+
+        private void SendResetPasswordEmail(string emailAddress)
+        {
+            //Generate a unique token for password reset
+            string resetToken = GenerateRandomToken();
+
+            //Store the reset token in the database along with user's email
+            StoreResetToekInDatabase(emailAddress, resetToken);
+
+            //Send the reset token to the user's email
+            SendEmail(emailAddress, resetToken);
+
+            //Show success message
+            MessageBox.Show("Password reset token has been sent to your email address.");
+
+            //Provide an input box for the user to enter the reset token
+            string inputToken = Microsoft.VisualBasic.Interaction.InputBox("Enter the reset token:", "Password Reset", "");
+
+            //Check if the entered token is correct
+            if(inputToken == resetToken)
+            {
+                MessageBox.Show("Token is correct. You can now reset your password.");
+            }
+            else
+            {
+                MessageBox.Show("Invalid reset token.");
+            }
+        }
+
+        //Generate a random token for password reset
+        private string GenerateRandomToken()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            return new string(Enumerable.Repeat(chars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        //Send email to user with password reset token
+        private void SendEmail(string emailAddress, string resetToken)
+        {
+            //Configure SMTP Client
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("fitnesstracker.1738@gmail.com", "Aung@2005123"),
+                EnableSsl = true
+            };
+
+            //Create email message
+            MailMessage message = new MailMessage("fitnesstracker.1738@gmail.com", emailAddress)
+            {
+                Subject = "Password Reset For Your Fitness Tracker Account",
+                Body = $"Your Password Reset Token is: {resetToken}"
+            };
+
+            //Send email
+            try
+            {
+                smtpClient.Send(message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error sending email: {ex.Message}");
+            }
+        }
+
+        //Store reset token in database
+        private void StoreResetToekInDatabase(string emailAddress, string resetToken)
+        {
+            // Store the reset token in the database
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                string query = "INSERT INTO password_reset_tokens (emailAddress, resetToken, expiryDate) VALUES (@emailAddress, @resetToken, @expiryDate)";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@emailAddress", emailAddress);
+                command.Parameters.AddWithValue("@resetToken", resetToken);
+                command.Parameters.AddWithValue("@expiryDate", DateTime.Now.AddDays(1)); // Token expire in 1 day
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error storing reset token in the database: {ex.Message}");
+                }
+            }
+        }
+
+        private void send_reset_link_Click(object sender, EventArgs e)
+        {
+            string emailAddress = reset_link_email.Text;
+
+            if (string.IsNullOrEmpty(emailAddress))
+            {
+                MessageBox.Show("Please enter your email address.");
+                return;
+            }
+
+            //Send the reset email to the User
+            SendResetPasswordEmail(emailAddress);
+        }
+    }
+}
