f5407f015c9083c089168abd1f1454c8c342690d
Aung Ye Htet <aung.atg1738@gmail.com>
2024-05-12 01:34:01 +0630

    Logic Updated!

--- FitnessTracker/CaloriesCalClass.cs
+++ FitnessTracker/CaloriesCalClass.cs
@@ -12,22 +12,40 @@ namespace FitnessTracker
     public class CaloriesCalClass
     {
         public string Name { get; set; }
-        public double Metric1 { get; set; }
-        public double Metric2 { get; set; }
-        public double Metric3 { get; set; }
+        public double TimesInMinutes { get; set; }
+        public double SpecificMetric { get; set; }
+        public double BodyWeight { get; set; }
+        
 
         public CaloriesCalClass(string name, double metric1, double metric2, double metric3)
         {
             this.Name = name;
-            this.Metric1 = metric1;
-            this.Metric2 = metric2;
-            this.Metric3 = metric3;
+            this.TimesInMinutes = metric1;
+            this.SpecificMetric = metric2;
+            this.BodyWeight = metric3;
         }
 
         public double CalculateCaloriesBurned()
         {
             double caloriesBurned = 0;
 
+            if(this.TimesInMinutes > 0 && this.BodyWeight > 0)
+            {
+                if(this.Name == "Walking")
+                {
+                    caloriesBurned = this.TimesInMinutes * GETMETVALUE(Name) * this.BodyWeight / 200;
+                }
+                else
+                {
+                    caloriesBurned = this.SpecificMetric * GETMETVALUE(Name) * this.BodyWeight / 200;
+                }
+            }
+
+            return caloriesBurned;
+        }
+
+        private double GETMETVALUE(string activityName)
+        {
             //Dictionary to store MET Values for different activities
             Dictionary<string, double> exerciseMETs = new Dictionary<string, double>()
             {
@@ -39,25 +57,14 @@ namespace FitnessTracker
                 { "Pull up", 5.8 },
             };
 
-            //Check if the exercise is present in the dictionary
-            if (exerciseMETs.ContainsKey(this.Name))
+            if(exerciseMETs.ContainsKey(activityName))
             {
-                //Retrieve MET value for the exercise
-                double MET = exerciseMETs[this.Name];
-
-                //Calculate calories burned based on exercise type
-                switch (this.Name)
-                {
-                    case "Walking":
-                        caloriesBurned = this.Metric2 * MET * this.Metric3 / 200; // M2(steps) * MET * M3(Duration) / 200
-                        break;
-                    default:
-                        caloriesBurned = this.Metric1 * MET * this.Metric2 * this.Metric3 / 60; //M1(times) * MET * M3(Duration) / 60
-                        break;
-                }
+                return exerciseMETs[activityName];
             }
-
-            return caloriesBurned;
+            else
+            {
+                return 0;
+            }
         }
     }
 }
